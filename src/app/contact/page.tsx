cc10d84d3642223569fb5145346121102267c8c4
Anton PC <anton.g@bachoodesign.com>
2025-10-07 11:35:04 +0200

    adjusmtents

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -2,7 +2,7 @@ import styles from "./page.module.scss";
 
 export default function Contact() {
     return (
-        <div className={styles.content} id="#contact">
+        <div className={styles.container} id="#contact">
             <img
                 src={"/team_standing.jpg"}
                 alt="Team standing"
@@ -11,25 +11,27 @@ export default function Contact() {
             <div className={styles.contact}>
                 <h1>Contacts </h1>
             </div>
-            <div className={styles.links}>
-                <span className={styles.email}>
-                    Facebook:{" "}
-                    <a href=" https://www.facebook.com/flow.ultimate.wroclaw">
-                        Flow Wroclaw Facebook
-                    </a>
-                </span>
-                <span className={styles.email}>
-                    Instagram:{" "}
-                    <a href="https://www.instagram.com/flow.ultimate.wroclaw/">
-                        Flow Wroclaw Instagram
-                    </a>
-                </span>
-                <span className={styles.email}>
-                    Email:{" "}
-                    <a href="mailto:flow.ultimate.wroclaw@gmail.com">
-                        flow.ultimate.wroclaw@gmail.com
-                    </a>
-                </span>
+            <div className={styles.content}>
+                <div className={styles.links}>
+                    <span className={styles.email}>
+                        Facebook:{" "}
+                        <a href=" https://www.facebook.com/flow.ultimate.wroclaw">
+                            Flow Wroclaw Facebook
+                        </a>
+                    </span>
+                    <span className={styles.email}>
+                        Instagram:{" "}
+                        <a href="https://www.instagram.com/flow.ultimate.wroclaw/">
+                            Flow Wroclaw Instagram
+                        </a>
+                    </span>
+                    <span className={styles.email}>
+                        Email:{" "}
+                        <a href="mailto:flow.ultimate.wroclaw@gmail.com">
+                            flow.ultimate.wroclaw@gmail.com
+                        </a>
+                    </span>
+                </div>
             </div>
         </div>
     );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,26 +10,31 @@ import Sponsors from "./sponsors/page";
 import Training from "./training/page";
 import Contact from "./contact/page";
 
+import grass from "../../public/grass.png";
+import team from "../../public/bg.jpg";
+
 export default function Home() {
     return (
-        <main className={styles.main}>
-            <img src={"/bg.jpg"} alt="Background 1" className={styles.team} />
-            <img src={"/grass.png"} alt="BG 2" />
+        <main
+            className={styles.main}
+            style={{ backgroundImage: `url(${team.src})` }}
+        >
+            {/* <img src={"/bg.jpg"} alt="Background 1" className={styles.team} /> */}
             <div className={styles.view}>
                 <div className={styles.content}>
                     <div className={styles.name}>K.S. Flow Wrocław</div>
                     <div className={styles.achievements}>
                         <InfoBlock
                             label={
-                                "Poland Mixed National Champions  \n (2021, 2019, 2018)"
+                                "Poland Mixed National Champions (2021, 2019, 2018)"
                             }
                         />
                         <InfoBlock
                             label={
-                                "Poland Beach National Champions \n (2021, 2015)"
+                                "Poland Beach National Champions (2021, 2015)"
                             }
                         />
-                        <InfoBlock label={"4th team in Europe \n (2021)"} />
+                        <InfoBlock label={"4th team in Europe (2021)"} />
                     </div>
                 </div>
                 <News />
