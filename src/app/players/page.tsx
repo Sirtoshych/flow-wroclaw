dfa8c9296225afb62c6934b86a5f247851716881
Anton PC <anton.g@bachoodesign.com>
2025-10-26 23:04:20 +0100

    adjust animation

--- src/app/players/page.tsx
+++ src/app/players/page.tsx
@@ -6,30 +6,37 @@ export default function Players() {
         <div className={styles.content} id="players">
             <div className={styles.container}>
                 <h1>Roster</h1>
-                <h2>Captain</h2>
-                <div className={styles.players}>
-                    <Player
-                        name="Pawel"
-                        lastname="Milanowicz"
-                        number={32}
-                        imgsrc={"Milanowicz1.jpg"}
-                        appearences={177}
-                        assists={466}
-                        goals={285}
-                    />
+                <div className={styles.captains}>
+                    <div className={styles.captain}>
+                        <h2>Captain</h2>
+                        <div className={styles.players}>
+                            <Player
+                                name="Pawel"
+                                lastname="Milanowicz"
+                                number={32}
+                                imgsrc={"Milanowicz1.jpg"}
+                                appearences={177}
+                                assists={466}
+                                goals={285}
+                            />
+                        </div>
+                    </div>
+                    <div className={styles.captain}>
+                        <h2>Spirit Captain</h2>
+                        <div className={styles.players}>
+                            <Player
+                                name="Sergey"
+                                lastname="Anufreiv"
+                                number={14}
+                                imgsrc={"Anufriev.jpg"}
+                                appearences={182}
+                                assists={137}
+                                goals={160}
+                            />
+                        </div>
+                    </div>
                 </div>
-                <h2>Spirit Captain</h2>
-                <div className={styles.players}>
-                    <Player
-                        name="Segey"
-                        lastname="Anufreiv"
-                        number={14}
-                        imgsrc={"Anufriev.jpg"}
-                        appearences={182}
-                        assists={137}
-                        goals={160}
-                    />
-                </div>
+
                 <h2>Players</h2>
                 <div className={styles.players}>
                     {PLAYERS.sort((a, b) => (a.number > b.number ? 1 : -1)).map(
